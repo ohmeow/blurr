2f1b533c3c265df5b0dfe96acfabab58dc463bd2
Wayde Gilliam <wgilliam@ohmeow.com>
2020-04-28 10:23:29 -0700

    fixed package name for pypi

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ lic = licenses[cfg['license']]
 min_python = cfg['min_python']
 
 setuptools.setup(
-    name = cfg['lib_name'],
+    name = 'ohmeow-blurr', #cfg['lib_name'],
     license = lic[0],
     classifiers = [
         'Development Status :: ' + statuses[int(cfg['status'])],
